d20f15ef16e87916962dc3e3552ccc73fa7aa1ad
unknown <ksufi7350@gmail.com>
2024-06-02 21:45:25 +0500

    homrpage

--- lib/AboutSection.dart
+++ lib/AboutSection.dart
@@ -8,14 +8,14 @@ class AboutSection extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SectionTitle(title: 'About Me'),
+          SectionTitle(title: 'About us'),
           Divider(
             // This creates a horizontal line
             color: const Color.fromARGB(255, 245, 240, 240),
             thickness: 1,
           ),
           Text(
-            "I'm a Muhammad Sufiyan third-year student studying Computer Science at Dawood University of Engineering & Technology. I've picked up a bunch of skills along the way, like making cool websites with WordPress, creating eye-catching designs, and coding in languages like C++, Java, and Python.\n\nEven though I haven't worked professionally yet, I'm super excited to use my love for tech to come up with awesome solutions. I'm always keeping an eye on the latest trends and breakthroughs, ready to jump in and learn something new.\n\nI'm all about teamwork too! I believe that when we work together, we can do amazing things – like turning ideas into real, working apps!",
+            "I'm a M.Sufiyan third-year student studying Computer Science at Dawood University of Engineering & Technology. I've picked up a bunch of skills along the way, like making cool websites with WordPress, creating eye-catching designs, and coding in languages like C++, Java, and Python.\n\nEven though I haven't worked professionally yet, I'm super excited to use my love for tech to come up with awesome solutions. I'm always keeping an eye on the latest trends and breakthroughs, ready to jump in and learn something new.\n\nI'm all about teamwork too! I believe that when we work together, we can do amazing things – like turning ideas into real, working apps!",
             style: TextStyle(color: Colors.white70, fontSize: 16),
           ),
         ],
