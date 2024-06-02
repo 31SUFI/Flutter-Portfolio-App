b04d52d52db37061e34eb3559330463558b2cce9
unknown <ksufi7350@gmail.com>
2024-06-02 21:39:50 +0500

    name changed

--- lib/AboutSection.dart
+++ lib/AboutSection.dart
@@ -14,23 +14,9 @@ class AboutSection extends StatelessWidget {
             color: const Color.fromARGB(255, 245, 240, 240),
             thickness: 1,
           ),
-          RichText(
-            text: TextSpan(
-              style: TextStyle(color: Colors.white70, fontSize: 16),
-              children: [
-                TextSpan(
-                  text: "I'm a ",
-                ),
-                TextSpan(
-                  text: "Muhammad Sufiyan",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                TextSpan(
-                  text:
-                      " third-year student studying Computer Science at Dawood University of Engineering & Technology. I've picked up a bunch of skills along the way, like making cool websites with WordPress, creating eye-catching designs, and coding in languages like C++, Java, and Python.\n\nEven though I haven't worked professionally yet, I'm super excited to use my love for tech to come up with awesome solutions. I'm always keeping an eye on the latest trends and breakthroughs, ready to jump in and learn something new.\n\nI'm all about teamwork too! I believe that when we work together, we can do amazing things – like turning ideas into real, working apps!",
-                ),
-              ],
-            ),
+          Text(
+            "I'm a Muhammad Sufiyan third-year student studying Computer Science at Dawood University of Engineering & Technology. I've picked up a bunch of skills along the way, like making cool websites with WordPress, creating eye-catching designs, and coding in languages like C++, Java, and Python.\n\nEven though I haven't worked professionally yet, I'm super excited to use my love for tech to come up with awesome solutions. I'm always keeping an eye on the latest trends and breakthroughs, ready to jump in and learn something new.\n\nI'm all about teamwork too! I believe that when we work together, we can do amazing things – like turning ideas into real, working apps!",
+            style: TextStyle(color: Colors.white70, fontSize: 16),
           ),
         ],
       ),
